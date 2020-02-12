cb2fe614dd985c913cad5b1b31ffe6f23ac86ec1
rifhall <rifhall@iu.edu>
2020-02-12 17:45:21 -0500

    these are the changes

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -4,5 +4,13 @@ extends KinematicBody2D
 func _ready():
 	pass
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("left"):
+		position.x = position.x - 10
+	if Input.is_action_pressed("right"):
+		position.x = position.x + 10
+	if Input.is_action_pressed("up"):
+		position.y = position.y - 10
+	if Input.is_action_pressed("down"):
+		position.y = position.y + 10
